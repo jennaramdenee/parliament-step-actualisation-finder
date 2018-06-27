7798ffc1775aef3abc8a5bdafdd0fab7fdb1eedf
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-06-27 12:43:35 +0100

    StepActualisationHelper tests

--- app/controllers/concerns/step_actualisation_helper.rb
+++ app/controllers/concerns/step_actualisation_helper.rb
@@ -4,19 +4,6 @@ module StepActualisationHelper
     ODataRequestHelper.request("ProcedureStep('#{step_id}')/ProcedureStepHasBusinessItem")["value"]
   end
 
-  def self.actualised_data(step_id, business_item)
-    business_item_id = business_item['LocalId']
-
-    work_package_id = get_work_package_id(step_id, business_item_id)
-    if work_package_id
-      work_packageable_thing_name = get_work_packageable_thing_name(step_id, business_item_id, work_package_id)
-    else
-      work_packageable_thing_name = nil
-    end
-
-    return work_package_id, work_packageable_thing_name
-  end
-
   # @return Either work package id or nil
   def self.get_work_package_id(step_id, business_item_id)
     work_package = ODataRequestHelper.request("ProcedureStep('#{step_id}')/ProcedureStepHasBusinessItem('#{business_item_id}')/BusinessItemHasWorkPackage")["value"]
@@ -29,9 +16,28 @@ module StepActualisationHelper
     work_packageable_thing['WorkPackageableThingName']
   end
 
-
   # @return Either step name or nil
   def self.get_procedure_step_name(step_id)
-    ODataRequestHelper.request("ProcedureStep('#{step_id}')")["value"].first['ProcedureStepName']
+    step_name = ODataRequestHelper.request("ProcedureStep('#{step_id}')")["value"]
+    step_name.any? ? step_name.first['ProcedureStepName'] : nil
+  end
+
+  # @return Either business item id or nil
+  def self.get_business_item_id(business_item)
+    business_item_id = business_item['LocalId']
+  end
+
+  def self.actualised_data(step_id, business_item)
+    # Exit if there is no business id
+    business_item_id = get_business_item_id(business_item)
+    return nil, nil unless business_item_id
+
+    # Exit if there is no work package id
+    work_package_id = get_work_package_id(step_id, business_item_id)
+    return nil, nil unless work_package_id
+
+    work_packageable_thing_name = get_work_packageable_thing_name(step_id, business_item_id, work_package_id)
+
+    return work_package_id, work_packageable_thing_name
   end
 end
--- spec/helpers/step_actualisation_helper_spec.rb
+++ spec/helpers/step_actualisation_helper_spec.rb
@@ -1,23 +1,141 @@
 require 'rails_helper'
 
-RSpec.describe StepActualisationHelper do
+RSpec.describe 'StepActualisationHelper' do
+  let(:valid_odata_response) {
+    {
+      "@odata.context": "https://api.parliament.uk/odata/$someMetaData",
+      "WorkPackageableThingName": "Equine Identification (England) Regulations 2018",
+      "value": [
+        {
+          "LocalId": "e9G2vHbc",
+          "ProcedureStepName": "Considered by the Joint Committee on Statutory Instruments (JCSI)"
+        }.stringify_keys
+      ]
+    }.stringify_keys
+  }
+
+  let(:empty_odata_response) {
+    {
+      "@odata.context": "https://api.parliament.uk/odata/$someMetaData",
+      "value": []
+    }.stringify_keys
+  }
+
+  let(:valid_business_item) {
+    {
+      "LocalId": "c5tH8nfB"
+    }.stringify_keys
+  }
+
+  let(:invalid_business_item) { {} }
+
   describe '#get_business_items' do
-
-  end
-
-  describe '#actualised_data' do
-
+    context 'making requests' do
+      it 'calls ODataRequestHelper' do
+        allow(ODataRequestHelper).to receive(:request).and_return(valid_odata_response)
+        expect(ODataRequestHelper).to receive(:request)
+        StepActualisationHelper.get_business_items('asdfzxcv')
+      end
+    end
   end
 
   describe '#get_work_package_id' do
+    context 'when work package exists' do
+      it 'returns work package id' do
+        allow(ODataRequestHelper).to receive(:request).and_return(valid_odata_response)
+        expect(StepActualisationHelper.get_work_package_id('step_id', 'business_item_id')).to eq('e9G2vHbc')
+      end
+    end
 
+    context 'when no work packages exist' do
+      it 'returns nil' do
+        allow(ODataRequestHelper).to receive(:request).and_return(empty_odata_response)
+        expect(StepActualisationHelper.get_work_package_id('step_id', 'business_item_id')).to eq(nil)
+      end
+    end
   end
 
   describe '#get_work_packageable_thing_name' do
+    context 'when work packageable thing name exists' do
+      it 'returns work package id' do
+        allow(ODataRequestHelper).to receive(:request).and_return(valid_odata_response)
+        expect(StepActualisationHelper.get_work_packageable_thing_name('step_id', 'business_item_id', 'work_package_id')).to eq('Equine Identification (England) Regulations 2018')
+      end
+    end
 
+    context 'when no work packageable thing name does not exist' do
+      it 'returns nil' do
+        allow(ODataRequestHelper).to receive(:request).and_return(empty_odata_response)
+        expect(StepActualisationHelper.get_work_packageable_thing_name('step_id', 'business_item_id', 'work_package_id')).to eq(nil)
+      end
+    end
   end
 
   describe '#get_procedure_step_name' do
+    context 'when procedure step name exists' do
+      it 'returns work package id' do
+        allow(ODataRequestHelper).to receive(:request).and_return(valid_odata_response)
+        expect(StepActualisationHelper.get_procedure_step_name('step_id')).to eq('Considered by the Joint Committee on Statutory Instruments (JCSI)')
+      end
+    end
 
+    context 'when no procedure step name does exists' do
+      it 'returns nil' do
+        allow(ODataRequestHelper).to receive(:request).and_return(empty_odata_response)
+        expect(StepActualisationHelper.get_procedure_step_name('step_id')).to eq(nil)
+      end
+    end
+  end
+
+  describe '#get_business_item_id' do
+    context 'when business item id exists' do
+      it 'returns work package id' do
+        expect(StepActualisationHelper.get_business_item_id(valid_business_item)).to eq('c5tH8nfB')
+      end
+    end
+
+    context 'when no business item id exists' do
+      it 'returns nil' do
+        expect(StepActualisationHelper.get_business_item_id(invalid_business_item)).to eq(nil)
+      end
+    end
+  end
+
+  describe '#actualised_data' do
+    context 'when all data exists' do
+      it 'returns work package id and work packageable thing name' do
+        allow(StepActualisationHelper).to receive(:get_business_item_id).and_return('business_item_id')
+        allow(StepActualisationHelper).to receive(:get_work_package_id).and_return('work_package_id')
+        allow(StepActualisationHelper).to receive(:get_work_packageable_thing_name).and_return('work_packageable_thing_name')
+
+        expect(StepActualisationHelper.actualised_data('step_id', valid_business_item)).to eq(['work_package_id', 'work_packageable_thing_name'])
+      end
+    end
+
+    context 'when not all data exists' do
+      context 'when business item id does not exist' do
+        it 'returns nil' do
+          allow(StepActualisationHelper).to receive(:get_business_item_id).and_return(nil)
+          expect(StepActualisationHelper.actualised_data('step_id', invalid_business_item)).to eq([nil, nil])
+        end
+      end
+
+      context 'when business item id exists but work package id does not' do
+        it 'returns nil' do
+          allow(StepActualisationHelper).to receive(:get_business_item_id).and_return('business_item_id')
+          allow(StepActualisationHelper).to receive(:get_work_package_id).and_return(nil)
+          expect(StepActualisationHelper.actualised_data('step_id', valid_business_item)).to eq([nil, nil])
+        end
+      end
+
+      context 'when business item id and work package id exists but work packageable thing name does not' do
+        it 'returns work package id, nil' do
+          allow(StepActualisationHelper).to receive(:get_business_item_id).and_return('business_item_id')
+          allow(StepActualisationHelper).to receive(:get_work_package_id).and_return('work_package_id')
+          allow(StepActualisationHelper).to receive(:get_work_packageable_thing_name).and_return(nil)
+          expect(StepActualisationHelper.actualised_data('step_id', valid_business_item)).to eq(['work_package_id', nil])
+        end
+      end
+    end
   end
 end
